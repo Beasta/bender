c89531bae078b621e51abb67f2a5645f0b378cef
beasta <barryblaha@gmail.com>
2017-09-13 10:58:41 -0700

    Add commands for setting directory and fullOrPart time

--- commands/directory.js
+++ commands/directory.js
@@ -1,21 +1,21 @@
 var request = require('request'),
-	util 	= require('../util');
+util 	= require('../util');
 
 module.exports = function (param) {
 	var	channel		= param.channel,
 		endpoint	= param.commandConfig.endpoint.replace('{gem}', param.args[0]),
 		info 		= [],
 		data = JSON.parse(fs.readFileSync(__dirname + '/../data.json', 'utf8'));
-	if (!data.day) {
-		data.day = 1;
+	if (!data.directory) {
+		data.directory = "";
 		fs.writeFileSync(__dirname + '/../data.json', JSON.stringify(data), 'utf8');
 	}
 	if (param.args[0]) {
-		data.day = param.args[0];
+		data.directory = param.args[0];
 		fs.writeFileSync(__dirname + '/../data.json', JSON.stringify(data), 'utf8');
-		info.push('set day to: ', param.args[0]);
+		info.push('set directory to: ', param.args[0]);
 	} else {
-		info.push(data.day);
+		info.push(data.directory);
 	}
 	util.postMessage(channel, info.join(''));
 };
--- commands/fullOrPart.js
+++ commands/fullOrPart.js
@@ -6,16 +6,20 @@ module.exports = function (param) {
 		endpoint	= param.commandConfig.endpoint.replace('{gem}', param.args[0]),
 		info 		= [],
 		data = JSON.parse(fs.readFileSync(__dirname + '/../data.json', 'utf8'));
-	if (!data.day) {
-		data.day = 1;
+	if (!data.fullOrPart) {
+		data.fullOrPart = 'fullTime';
 		fs.writeFileSync(__dirname + '/../data.json', JSON.stringify(data), 'utf8');
 	}
 	if (param.args[0]) {
-		data.day = param.args[0];
-		fs.writeFileSync(__dirname + '/../data.json', JSON.stringify(data), 'utf8');
-		info.push('set day to: ', param.args[0]);
+		if (param.args[0] === 'fullTime' || param.args[0] === 'partTime') {
+			data.fullOrPart = param.args[0];
+			fs.writeFileSync(__dirname + '/../data.json', JSON.stringify(data), 'utf8');
+			info.push('set schedule to: ', param.args[0]);
+		} else {
+			info.push('the only two options are "fullTime" or  "partTime"')
+		}
 	} else {
-		info.push(data.day);
+		info.push(data.fullOrPart);
 	}
 	util.postMessage(channel, info.join(''));
 };
